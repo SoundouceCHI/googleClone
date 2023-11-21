e5ba4bfae565eb7e6d7bd0ef3137d40a7ef4efc5
SoundouceCHI <soundouce.chibani@gmail.com>
2023-11-21 12:52:14 +0100

    Create search result component

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,6 +1,10 @@
+import WebSearchResults from "@/component/WebSearchResults";
 import Link from "next/link";
+import { resolve } from "styled-jsx/css";
 
 export default async function WebSearchPage({searchParams}) {
+  await new Promise((resolve)=> setTimeout(resolve, 10000)); 
+
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
@@ -20,7 +24,7 @@ export default async function WebSearchPage({searchParams}) {
   }
 
   return <>
-    {results && results.map(result=> <h1>{result.title}</h1>) }
+    {results && <WebSearchResults results= {data}/> }
   </>
   
 }
