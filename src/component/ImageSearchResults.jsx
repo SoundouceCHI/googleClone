e730cbaad0657813d068efb5096b1a8aa319aac3
SoundouceCHI <soundouce.chibani@gmail.com>
2023-11-24 12:07:02 +0100

    Add pagination component

--- src/component/ImageSearchResults.jsx
+++ src/component/ImageSearchResults.jsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
+import PaginationButtons from './PaginationButtons'
 
 export default function ImageSearchResults({results}) {
 
   return (
-    <div className='pb-24 pt-4'>
+    <div className='sm:pb-24 pb-40 pt-4'>
         <div className='grid grid-cols-1 sm:grid-cols-2 
         lg:grid-cols-3 xl:grid-cols-4 px-3 space-x-4'>
             {results.items.map((result)=>( 
@@ -24,6 +25,9 @@ export default function ImageSearchResults({results}) {
                     </div>
                 </div>
             ))}
+        </div>
+        <div className='ml-16'>
+            <PaginationButtons/>
         </div>
     </div>
   )
--- src/component/WebSearchResults.jsx
+++ src/component/WebSearchResults.jsx
@@ -1,8 +1,9 @@
 import Link from "next/link"
 import Parser from "html-react-parser"
+import PaginationButtons from "./PaginationButtons"
 export default function WebSearchResults({results}) {
   return (
-    <div  className="w-full mx-auto px-3 pb-24 sm:pl-[5%]
+    <div  className="w-full mx-auto px-3 pb-40 sm:pb-24 sm:pl-[5%]
     md:pl-[14%] lg:pl-52">
         <p className="text-gray-600 text-sm mb-5 mt-3">
             About {results.searchInformation?.formattedTotalResults} results 
@@ -24,6 +25,9 @@ export default function WebSearchResults({results}) {
                 </p>
             </div>
         )}
+        <div>
+            <PaginationButtons/>
+        </div>
     </div>
   )
 }
